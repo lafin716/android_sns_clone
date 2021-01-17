02f8337d5c98ccea903dfeb6b0ff10db37e1b8c9
lafinco <lafin716@naver.com>
2021-01-18 05:14:58 +0900

    first commit

---
+++ app/src/main/java/com/lafin/firebase_sns_clone/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lafin.firebase_sns_clone.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lafin.firebase_sns_clone.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/lafin/firebase_sns_clone/activity/SignUpActivity.java
@@ -0,0 +1,20 @@
+package com.lafin.firebase_sns_clone.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.lafin.firebase_sns_clone.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_signup);
+    }
+
+
+}
